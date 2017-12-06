452050d9070894b5a7d8a4e37a562feff7b6e4dc
0x0 <ghstoreinvesting@gmail.com>
2017-12-06 16:15:23 +0800

    Add showing location starting code

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -20,3 +20,32 @@ module.exports.bedVacanciesList = function(req, res) {
 		]
 	});
 }
+
+module.exports.showLocation = function(req, res) {
+	res.render('showLocation', {
+		title: 'Mission Australia', 
+		tagline: 'Bringing people together',
+		location: {
+			info: 'Located in East Perth',
+			opening: '9am',
+			closing: '5pm',
+			facilities: [
+				'Hot Drinks',
+				'Wifi',
+				'Personal Rooms',
+				'Councilors'
+			],
+			restrictions: [
+				'10pm curfew',
+				'No drugs',
+				'No alcohol',
+				'No medical issues'
+			]
+		},
+		map: {
+			title: 'Mission Australia Service',
+			url: 'https://maps.googleapis.com/maps/api/staticmap?center=40.711614,-74.012318&size=800x800&&markers=color:green%7Clabel:G%7C40.711614,-74.012318&zoom=13'
+		},
+		additionalInfo: 'Every day we support people nationwide by combatting homelessness, assisting disadvantaged families and children, addressing mental health issues, fighting substance dependencies, and much more. We’re generously supported by our funders, partners and tens of thousands of everyday Australians, who make the work of our tireless volunteers and staff possible.'
+	})
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,5 @@ var ctrlLocations = require('../controllers/locations');
 
 /** Make the home page render a list of bed vaccancies for now  */
 router.get('/', ctrlLocations.bedVacanciesList);
-
+router.get('/location', ctrlLocations.showLocation);
 module.exports = router;
