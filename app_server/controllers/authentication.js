5d4eb146bf22d77e2cc7318948ecef978206cf3c
0x0 <ghstoreinvesting@gmail.com>
2017-12-09 12:25:59 +0800

    Fix build breaking code

--- app_server/controllers/authentication.js
+++ app_server/controllers/authentication.js
@@ -7,34 +7,34 @@ var sendJSONresponse = function(res, status, content) {
   res.json(content);
 };
 
-// module.exports.register = function(req, res) {
-//   if(!req.body.name || !req.body.email || !req.body.password) {
-//     sendJSONresponse(res, 400, {
-//       "message": "All fields required"
-//     });
-//     return;
-//   }
-//
-//   var user = new User();
-//
-//   user.name = req.body.name;
-//   user.email = req.body.email;
-//
-//   user.setPassword(req.body.password);
-//
-//   user.save(function(err) {
-//     var token;
-//     if (err) {
-//       sendJSONresponse(res, 404, err);
-//     } else {
-//       token = user.generateJwt();
-//       sendJSONresponse(res, 200, {
-//         "token" : token
-//       });
-//     }
-//   });
-//
-// };
+module.exports.register = function(req, res) {
+  if(!req.body.name || !req.body.email || !req.body.password) {
+    sendJSONresponse(res, 400, {
+      "message": "All fields required"
+    });
+    return;
+  }
+
+  var user = new User();
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    var token;
+    if (err) {
+      sendJSONresponse(res, 404, err);
+    } else {
+      token = user.generateJwt();
+      sendJSONresponse(res, 200, {
+        "token" : token
+      });
+    }
+  });
+
+};
 
 module.exports.login = function(req, res) {  
   if(!req.body.email || !req.body.password) {
