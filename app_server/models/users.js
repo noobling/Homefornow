e768e0df0b8080f8e5f9d6982bb52fa607611b3a
0x0 <ghstoreinvesting@gmail.com>
2017-12-24 22:57:09 +0800

    Revert "Clean up user.js"
    This reverts commit b351be1f38fba9aa2433f315bb3f15b1514a194c.

--- app_server/models/users.js
+++ app_server/models/users.js
@@ -1,31 +1,49 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+var mongoose = require( 'mongoose' );
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-// Primary user, person seeking accomodation
-const userSchema = new mongoose.Schema({
+//Primary user, person seeking accomodation
+var userSchema = new mongoose.Schema({
+  //email of user
   email: {
     type: String,
-    unique: true,
+    unique: true
   },
+  //name of user
   name: String,
+  //hash of their password
   hash: String,
+  //salt to the hash for the password
   salt: String,
-  facebook: {
+  //If user signs in using Facebook
+  facebook : {
     id: String,
     token: String,
     email: String,
     name: String,
-  },
+  }
 });
 
-userSchema.methods.setPassword = (password) => {
+userSchema.methods.setPassword = function(password){
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 };
 
-userSchema.methods.validPassword = (password) => {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
   return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, 'secret'); // In real app the secret should be a long random string kept in environment variables
 };
 
 mongoose.model('User', userSchema);
