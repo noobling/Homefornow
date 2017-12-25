bdd39ff4329591167a5beac9fcdb8f47ada8df66
0x0 <ghstoreinvesting@gmail.com>
2017-12-25 11:39:28 +0800

    refactor authentication tests

--- test/integration/routes.authentication.test.js
+++ test/integration/routes.authentication.test.js
@@ -1,16 +1,14 @@
 const chai = require('chai');
-const should = chai.should();
 const chaiHttp = require('chai-http');
-let request = require('supertest');
-const fs = require('fs');
-const path = require('path');
+const request = require('supertest');
 const users = require('../../seeded_users.json');
+
+const should = chai.should();
 chai.use(chaiHttp);
 
 const server = require('../../app');
 
 describe('routes : authentication', () => {
-
   beforeEach((done) => {
     done();
   });
@@ -22,46 +20,46 @@ describe('routes : authentication', () => {
   describe('POST /login', () => {
     it('should login with valid credentials', (done) => {
       request.agent(server)
-      .post('/login')
-      .send({
-        email: users[0].email,
-        password: users[0].password
-      })
-      .redirects(1)
-      .end((err, res) => {
-        should.not.exist(err);
-        res.status.should.equal(200);
-        res.text.should.contain("Welcome "+users[0].name);
-        done();
-      });
+        .post('/login')
+        .send({
+          email: users[0].email,
+          password: users[0].password,
+        })
+        .redirects(1)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.status.should.equal(200);
+          res.text.should.contain(`Welcome ${users[0].name}`);
+          done();
+        });
     });
 
-    it ('should fail to login with invalid credentials', (done) => {
+    it('should fail to login with invalid credentials', (done) => {
       request.agent(server)
-      .post('/login')
-      .send({
-        email: 'randomemail@email.com',
-        password: 'invalidpass'
-      })
-      .redirects(1)
-      .expect(200)
-      .end((err, res) => {
-        should.not.exist(err);
-        res.text.should.contain('Incorrect username');
-        done();
-      });
+        .post('/login')
+        .send({
+          email: 'randomemail@email.com',
+          password: 'invalidpass',
+        })
+        .redirects(1)
+        .expect(200)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.text.should.contain('Incorrect username');
+          done();
+        });
     });
   });
 
   describe('GET /404', () => {
     it('should throw an error', (done) => {
       chai.request(server)
-      .get('/404')
-      .end((err, res) => {
-        res.redirects.length.should.equal(0);
-        res.status.should.equal(404);
-        done();
-      });
+        .get('/404')
+        .end((err, res) => {
+          res.redirects.length.should.equal(0);
+          res.status.should.equal(404);
+          done();
+        });
     });
   });
 
