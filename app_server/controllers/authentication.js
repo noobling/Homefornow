0a7d6d788e007d54d9eccdb15fd3970fe9863908
0x0 <ghstoreinvesting@gmail.com>
2017-12-27 23:27:24 +0800

    Revert "Revert "refactor code to conform with eslint""
    This reverts commit 39f37c74c4877887dc3a4e1936abd782cca2d02e.

--- app_server/controllers/authentication.js
+++ app_server/controllers/authentication.js
@@ -1,63 +1,60 @@
-var passport = require('passport');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+const passport = require('passport');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('User');
 // This builds a json response
 // Generally you should call `return` after this
 // function
-var sendJSONresponse = function(res, status, content) {
+function sendJSONresponse(res, status, content) {
   res.status(status);
   res.json(content);
-};
+}
 
-module.exports.register = function(req, res) {
-  if(!req.body.name || !req.body.email || !req.body.password) {
+module.exports.register = (req, res) => {
+  if (!req.body.name || !req.body.email || !req.body.password) {
     sendJSONresponse(res, 400, {
-      "message": "All fields required"
+      message: 'All fields required',
     });
     return;
   }
 
-  var user = new User();
+  const user = new User();
 
   user.name = req.body.name;
   user.email = req.body.email;
 
   user.setPassword(req.body.password);
 
-  user.save(function(err) {
-    var token;
+  user.save((err) => {
+    let token;
     if (err) {
       sendJSONresponse(res, 404, err);
     } else {
       token = user.generateJwt();
       sendJSONresponse(res, 200, {
-        "token" : token
+        token,
       });
     }
   });
-
 };
 
-module.exports.login = function(req, res) {
-  if(!req.body.email || !req.body.password) {
+module.exports.login = (req, res) => {
+  if (!req.body.email || !req.body.password) {
     sendJSONresponse(res, 400, {
-      "message": "All fields required"
+      message: 'All fields required',
     });
     return;
   }
 
-  passport.authenticate('local', function(err, user, info){
-    var token;
-
+  passport.authenticate('local', (err, user, info) => {
     if (err) {
       sendJSONresponse(res, 404, err);
       return;
     }
 
-    if(user){
-			var sess = req.session;
-			sess.user = user;
+    if (user) {
+      let sess = req.session;
+      sess.user = user;
       res.redirect('/service');
     } else {
       sendJSONresponse(res, 401, info);
