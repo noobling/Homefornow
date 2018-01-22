0f6d0313104b819c236d430c06c2f76dffba82a8
Alex Arnold <21304455@student.uwa.edu.au>
2018-01-22 18:07:38 +0800

    Made image carousel dynamically generated from images stored in Firebase
    This will require the JSON file uploaded to Slack to verify/authenticate
    the Firebase Admin API

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const admin = require('firebase-admin');
 
 const Accommodation = mongoose.model('Accommodation');
 const Request = mongoose.model('Request');
@@ -61,25 +62,67 @@ module.exports.longTermList = (req, res) => {
 };
 
 module.exports.showLocation = (req, res) => {
+  let metadataCount = 0;
+  let listCount = 0;
   console.log('Id: '.concat(req.params.accommodationId));
   Accommodation.findById(
     req.params.accommodationId,
-    'name tagline address.suburb facilities restrictions additionalInfo website',
+    'name tagline address.suburb facilities restrictions additionalInfo website img',
   ).exec()
     .then((accommodation) => {
       console.log('Doc: '.concat(accommodation));
-      res.render('showLocation', {
-        location: accommodation,
-        map: {
-          title: accommodation.name,
-          suburb: accommodation.address.suburb,
-        },
-        images: [
-          'https://firebasestorage.googleapis.com/v0/b/homefornow-fd495.appspot.com/o/images%2Fyass%2Fimage1.jpg?alt=media&token=257f0433-4750-4afa-bcb0-a1d2291e5651',
-          'https://firebasestorage.googleapis.com/v0/b/homefornow-fd495.appspot.com/o/images%2Fyass%2Fimage2.jpg?alt=media&token=1da53563-4c44-4882-a4a3-e5d3628177e0',
-          'https://firebasestorage.googleapis.com/v0/b/homefornow-fd495.appspot.com/o/images%2Fyass%2Fimage3.jpg?alt=media&token=32fbef96-c319-418b-9e29-edba901b441f',
-        ],
-      });
+
+      console.log('Images: '.concat(accommodation.img));
+      const imageList = [];
+
+      if (accommodation.img != null && accommodation.img.length > 0) {
+        listCount = accommodation.img.length;
+        const bucket = admin.storage().bucket();
+
+        accommodation.img.forEach((image) => {
+          // Get the metadata for each image reference
+          bucket.file(image).getMetadata().then((data) => {
+            // Add the media link for the image to 'imageList'
+            imageList[imageList.length] = data[0].mediaLink;
+            metadataCount += 1;
+
+            // If all the images have been added to imageList, render the page with these images
+            if (metadataCount === listCount) {
+              res.render('showLocation', {
+                location: accommodation,
+                map: {
+                  title: accommodation.name,
+                  suburb: accommodation.address.suburb,
+                },
+                images: imageList,
+              });
+            }
+          }).catch((err) => {
+            console.log('[ERROR] LocationsController: '.concat(err));
+
+            // If image metadata cannot be obtained, then load the page without the images panel
+            listCount -= 1;
+            if (metadataCount === listCount) {
+              res.render('showLocation', {
+                location: accommodation,
+                map: {
+                  title: accommodation.name,
+                  suburb: accommodation.address.suburb,
+                },
+              });
+            }
+          });
+        });
+      } else {
+        // If 'img' is not defined or is empty, render the page without the carousel
+        res.render('showLocation', {
+          location: accommodation,
+          map: {
+            title: accommodation.name,
+            suburb: accommodation.address.suburb,
+          },
+        });
+      }
     })
     .catch((err) => {
       console.log('[ERROR] LocationsController: '.concat(err));
