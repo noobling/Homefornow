4e721e29ae2966d2d526333d0f703a3a5adc0e1f
SirSlothalot <matthew.stagoll@gmail.com>
2018-02-01 09:37:01 +0800

    Cleaned some code.

--- generate.seed.data.js
+++ generate.seed.data.js
@@ -13,7 +13,6 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-
 /**
  * HELPER FUNCTIONS
  */
@@ -33,22 +32,39 @@ function writeTofile(data) {
   );
 }
 
+/**
+ * Returns a random integer between min and max. Includes min and max.
+ * @param  {number} min Minimum random number possible.
+ * @param  {number} max Maximum random number posisble.
+ * @return {number}     Random number between min and max.
+ */
+function randInt(min, max) {
+  const range = (max - min) + 1;
+  return Math.floor(Math.random() * range) + min;
+}
+
+/**
+ * Returns a random element from the specified array.
+ * @param  {Array} arr The array to return an element from.
+ * @return {*}     A random element from arr.
+ */
+function choose(arr) {
+  return arr[randInt(0, arr.length - 1)];
+}
 
 /**
  * SEED THE USERS
  */
 const USER_ROLE = 'youth';
 const NUM_USERS = 2;
-const GENDER = ['Male', 'Female', 'Other'];
+const GENDER = User.schema.path('gender').enumValues;
 
 const seededUsers = [];
 function seedUsers() {
   for (let i = 0; i < NUM_USERS; i += 1) {
-    const year = Math.floor(2003 - (Math.random() * 10)); // 15 to 25 y.o. as of 2018
-    const month = Math.floor((Math.random() * 12) + 1);
-    const day = Math.floor((Math.random() * 30) + 1); // assume 30 days in month
-
-    // console.log('DOB: ' + day + '-' + month + '-' + year);
+    const year = Math.floor(2003 - randInt(0, 10)); // 15 to 25 y.o. as of 2018
+    const month = randInt(1, 12);
+    const day = randInt(1, 30);
 
     const user = new User();
 
@@ -58,10 +74,10 @@ function seedUsers() {
       email: faker.Internet.email(),
       phoneNumber: faker.PhoneNumber.phoneNumberFormat(0),
       dob: new Date().setFullYear(year, month, day),
-      gender: GENDER[Math.floor(Math.random() * GENDER.length)],
+      gender: choose(GENDER),
       hasDisability: Math.random() < 0.5,
-      password: faker.Internet.userName(),
-      role: USER_ROLE
+      role: USER_ROLE,
+      password: faker.Internet.userName()
     };
 
     user.firstName = seedUser.firstName;
@@ -71,8 +87,8 @@ function seedUsers() {
     user.dob = seedUser.dob;
     user.gender = seedUser.gender;
     user.hasDisability = seedUser.hasDisability;
-    user.setPassword(seedUser.password);
     user.role = seedUser.role;
+    user.setPassword(seedUser.password);
 
     user.save((err) => {
       if (err) {
