936e570313d501a0e3118d0305b73ed4e29d37d6
SirSlothalot <matthew.stagoll@gmail.com>
2018-02-13 02:05:51 +0800

    Improved location search. Can search 'subi' instead of 'Subiaco WA, Australia'.

--- public/javascripts/other.js
+++ public/javascripts/other.js
@@ -20,82 +20,8 @@ function initialize()
 }
 
 /**
- * [getCoords description]
- * @param  {Object} autocomplete Google places autocomplete object
- * @return {Object}              JSON object containing lat and long coordinates.
- */
-function getCoords(autocomplete)
-{
-  const place = autocomplete.getPlace();
-
-  if(!place) return;
-
-  return coords = {
-    lat: place.geometry.location.lat(),
-    long: place.geometry.location.lng()
-  }
-}
-
-/**
- * Copies the latitude and longitude from the Google places autocomplete
- * object into a form.
- * @param  {Object}   autocomplete  Google places autocomplete object
- * @param  {Boolean}  isLongTerm    True if searching for long term accommodation, false otherwise.
- */
-function search(autocomplete, isLongTerm) // Fills form with lat and long from the Google Maps API
-{
-  const coords = getCoords(autocomplete);
-
-  if(!coords) {
-    console.log('Invalid location.');
-    return;
-  }
-
-  const user = document.getElementById('userId');
-  const lengthOfStay = isLongTerm ? 'long_term' : 'short_term';
-
-  if(user) {
-    // Create a form to submit lat and long
-    const form = document.createElement('form');
-    const lat = document.createElement('input');
-    const long = document.createElement('input');
-
-    lat.name = 'lat';
-    long.name = 'long';
-
-    lat.value = coords.lat;
-    long.value = coords.long;
-
-    form.appendChild(lat);
-    form.appendChild(long);
-
-    form.method = 'POST';
-    form.action = '/locations/' + lengthOfStay;
-
-    document.body.appendChild(form);
-
-    form.submit();
-  } else {
-    // Append lat and long to login and register forms
-    loginForm = document.getElementById('loginForm');
-    regForm = document.getElementById('registerForm');
-
-    loginForm.elements['lat'].value = coords.lat;
-    loginForm.elements['long'].value = coords.long;
-    loginForm.elements['lengthOfStay'].value = lengthOfStay;
-
-    regForm.elements['lat'].value = coords.lat;
-    regForm.elements['long'].value = coords.long;
-    regForm.elements['lengthOfStay'].value = lengthOfStay;
-
-    $('#regloginmodal').modal('show');
-  }
-}
-
-/**
  * 'Use Current Location' functionality.
- * Gets the user's current latitude and longitude coordinates
- * and passes them into a form.
+ * Gets the user's current latitude and longitude coordinates.
  */
 function geoloc()
 {
@@ -112,8 +38,6 @@ function geoloc()
     {
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
-        // document.getElementById('lat').value = lat;
-        // document.getElementById('long').value = lng;
 
         const geocoder = new google.maps.Geocoder;
         geocoder.geocode( { 'address': lat + ', ' + lng }, (results, status) => {
@@ -149,35 +73,101 @@ function geoloc()
     navigator.geolocation.getCurrentPosition(success, error);
 }
 
-// /**
-//  * Input location functionality.
-//  * Gets the address typed in by the user and
-//  * sends the latitude and longitude into a form.
-//  */
-// $(document).ready(() => {
-//     $('#location').change(() => {
-//         const lat = document.getElementById('lat').value;
-//         const lng = document.getElementById('long').value;
-//         let templat, templong;
+/**
+ * Create and submit a form with the searched lat, long and length of stay.
+ * @param  {Object} coords       JSON object containing lat and long coordinates of searched location.
+ * @param  {String} lengthOfStay If the user is searching for 'short_term' or 'long_term' accommodation.
+ */
+function signedInSearch(coords, lengthOfStay)
+{
+  const form = document.createElement('form');
+  const lat = document.createElement('input');
+  const long = document.createElement('input');
+
+  lat.name = 'lat';
+  long.name = 'long';
+
+  lat.value = coords.lat;
+  long.value = coords.long;
+
+  form.appendChild(lat);
+  form.appendChild(long);
+
+  form.method = 'POST';
+  form.action = '/locations/' + lengthOfStay;
+
+  document.body.appendChild(form);
+
+  form.submit();
+}
+
+/**
+ * Append lat, long and length of stay to the login and register forms,
+ * then prompt user to login or register.
+ * @param  {Object} coords       JSON object containing lat and long coordinates of searched location.
+ * @param  {String} lengthOfStay If the user is searching for 'short_term' or 'long_term' accommodation.
+ */
+function unsignedInSearch(coords, lengthOfStay)
+{
+  const loginForm = document.getElementById('loginForm');
+  const regForm = document.getElementById('registerForm');
+
+  loginForm.elements['lat'].value = coords.lat;
+  loginForm.elements['long'].value = coords.long;
+  loginForm.elements['lengthOfStay'].value = lengthOfStay;
+
+  regForm.elements['lat'].value = coords.lat;
+  regForm.elements['long'].value = coords.long;
+  regForm.elements['lengthOfStay'].value = lengthOfStay;
+
+  $('#regloginmodal').modal('show');
+}
+
+/**
+ * Copies the latitude and longitude from the Google places autocomplete
+ * object into a form.
+ * @param  {Object}   autocomplete  Google places autocomplete object
+ * @param  {Boolean}  isLongTerm    True if searching for long term accommodation, false otherwise.
+ */
+function search(autocomplete, isLongTerm) // Fills form with lat and long from the Google Maps API
+{
+  const user = document.getElementById('userId');
+  const lengthOfStay = isLongTerm ? 'long_term' : 'short_term';
+
+  const address = document.getElementById('location').value;
+  if (!address) { return; }
+
+  const geocoder = new google.maps.Geocoder;
+  const request = {
+    address: address,
+    componentRestrictions: {
+      country: 'AUSTRALIA'
+    }
+  }
+
+  geocoder.geocode( request, (results, status) => {
+    if (status == 'OK') {
+      coords = {
+        lat: results[0].geometry.location.lat(),
+        long: results[0].geometry.location.lng()
+      }
+      if(user) {
+        signedInSearch(coords, lengthOfStay);
+      } else {
+        unsignedInSearch(coords, lengthOfStay);
+      }
+    } else {
+      console.log('Geocode was not successful for the following reason: ' + status);
+      return;
+    }
+  });
+}
+
+// const place = autocomplete.getPlace();
 //
-//         const address = document.getElementById('location').value;
-//         const geocoder = new google.maps.Geocoder;
-//         geocoder.geocode( { 'address': address }, (results, status) => {
-//             if (status == 'OK')
-//             {
-//                 templat = results[0].geometry.location.lat();
-//                 templong = results[0].geometry.location.lng();
+// if(!place) return;
 //
-//                 if( lat != templat || lng != templong )
-//                 {
-//                     document.getElementById('lat').value = templat;
-//                     document.getElementById('long').value = templong;
-//                 }
-//             }
-//             else
-//             {
-//                 console.log('Geocode was not successful for the following reason: ' + status);
-//             }
-//         });
-//     })
-// })
+// return coords = {
+//   lat: place.geometry.location.lat(),
+//   long: place.geometry.location.lng()
+// }
